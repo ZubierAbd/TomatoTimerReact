e126fc0c9b26452277a0b60247bacf315f84b6f1
Zubier <zubierabd@gmail.com>
2020-09-19 21:10:48 -0400

    made minor css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Favicon from '../assets/favicon.png'
 
 const Header = ({ title }) => {
     return (
         <div className="panel">
             <header className="header">
-                <h2>{title}</h2>
+                <span className="tomatoHeader"><img src={Favicon}></img></span>   <h2>{title}</h2>        <span className="tomatoHeader"><img src={Favicon}></img></span>
             </header>
 
-        </div>
+        </div >
     )
 }
 
